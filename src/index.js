d7215a6b658b67c504993a602424fd8a2a02ec0d
Dzima <d.propolsky@gmail.com>
2020-10-19 16:04:34 +0300

    feat: add solution of human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,38 +1,44 @@
 module.exports = function toReadable (number) {
-  (number).toString().split('') //разбиваю число на элементы массива
-  // разделить число чтобы определить десятки, сотни, тысячи и т.д.
-  let numberValue = number;
-  let tens = numberValue%10; //десятки 
-  
-  numberValue = (numberValue - tens)/10
-  let hundreds = numberValue%10; // сотни
-  numberValue = (numberValue - hundreds)/10      
-  let thousands = numberValue%10; // тысячи
-  numberValue = (numberValue - thousands)/10
-  const nameOfTens = (tens) => {
-      switch (tens) {
-        case 1: tens = 'one';
-            break
-        case 2: tens = 'two';
-            break
-        case 3: tens = 'three';       
-            break
-        case 4: tens = 'four';     
-            break
-        case 5: tens = 'five';     
-            break
-        case 6: tens = 'six';
-            break
-        case 7: tens = 'seven';
-            break
-        case 8: tens = 'eight';
-            break
-        case 9: tens = 'nine';
-            break
-        default: tens = '';
-            break         
-      }
-      return tens;
-}
-  console.log(tens, hundreds, thousands, numberValue);
+    // разделить число чтобы определить десятки, сотни, тысячи и т.д.
+    let numberValue = number;
+    let numeral = numberValue % 10; //0-9            //4
+    numberValue = (numberValue - numeral) / 10       
+    let tens = numberValue % 10; // 10-19            //3
+    numberValue = (numberValue - tens) / 10          
+    let hundred = numberValue % 10; // сотни        //2
+    numberValue = (numberValue - hundred) / 10       
+    
+    let numeralArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let teensArray = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tensArray = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+           
+
+    // сотни
+    let hundredText; 
+    if (hundred !== 0) {  // если сотни есть
+        hundredText = `${numeralArray[hundred]} hundred`;
+    } else {
+        hundredText = '';
+    }       
+    
+    // десятки
+    let tensText;
+    if (tens === 1) { // 10-19 
+        tensText = teensArray[numeral];
+    } else if (tens !== 0 && tens !== 1) { //20-99
+        tensText = tensArray[tens];
+    } else if (tens === 0){
+        tensText = '';
+    }
+
+    // единицы 
+    let numeralText;
+    if ((tens === 1) || ((hundred !== 0 || tens !== 0) &&  numeral === 0)) {
+        numeralText = '';
+    } else if (((hundred !== 0 || (tens !== 0 && tens !== 1)) && numeral !== 0) || ((hundred == 0 || tens == 0) && (numeral === 0 || numeral !== 0))) {
+        numeralText = numeralArray[numeral];
+    } 
+
+    let resultArray = [hundredText, tensText, numeralText];
+    return resultArray.filter(element => element !== '').join(' ');
 }
